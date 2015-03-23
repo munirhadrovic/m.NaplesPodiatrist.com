4641c6cbbd587bc757b4c02bb018f880bf6ee951
novicaa <novicamiketic@gmail.com>
2015-03-23 14:13:10 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="Family Foot and Leg Center – North Naples Royal Palm Medical Building 1660 Medical Blvd, Suite #302 Naples, FL 34110"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone="2394303668" ;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
